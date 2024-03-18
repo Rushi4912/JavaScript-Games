2dd9106666faa083b4019047a2a8a8350d47573c
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-03-18 22:09:52 +0530

    change

--- snake/script.js
+++ snake/script.js
@@ -12,9 +12,14 @@ function drawSnake() {
      snake.forEach((segment) => {
           const snakeElement = createGameElement('div', 'snake');
      });
+     setPosition(snakeElement,segment);
 }
 
 function createGameElement(tag,className) {
      const element = document.createElement(tag);
      element.className = className;
+     return element;
+}
+function setPosition(element, position) {
+     element.style.gridColumn = position.x;
 }
