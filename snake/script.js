b7d1fc76582bfd822f9a140fe2e507016392106a
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-03-18 22:03:17 +0530

    draw snake

---
+++ snake/script.js
@@ -0,0 +1,13 @@
+// define html element
+const board = document.getElementById("game-board");
+let snake = [{ x: 10, y: 10 }];
+
+function draw(){
+
+     board.innerHTML = '';
+     drawSnake();
+}
+// draw snake
+function drawSnake() {
+     
+}
