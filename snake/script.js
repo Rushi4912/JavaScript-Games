0af1aca348c400b8929ad340d674d4c6fe6a407a
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-03-20 22:27:54 +0530

    chnage

--- snake/script.js
+++ snake/script.js
@@ -4,42 +4,40 @@ let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
 let gridSize = 20;
 
-function draw(){
-
+function draw() {
      board.innerHTML = '';
-     drawSnake();
-}
-// draw snake
-function drawSnake() {
-     snake.forEach((segment) => {
-          const snakeElement = createGameElement('div', 'snake');
-           setPosition(snakeElement, segment);
-           board.appendChild(snakeElement);
-     });
-
-}
-
-function createGameElement(tag,className) {
-     const element = document.createElement(tag);
-     element.className = className;
-     return element;
-
-}
-function setPosition(element, position) {
-     element.style.gridColumn = position.x;
-     element.style.gridRow = position.y;
-}
-// draw();
-
-// draw function food
-function drawFood() {
-     const foodElement = createGameElement('div', 'food');
-     setPosition(foodElement, food);
+     
      board.appendChild(foodElement);
+  drawFood();
+}
+     return { x,};
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement('div', 'snake');
+    setPosition(snakeElement, segment);
+    board.appendChild(snakeElement);
+  });
+}
+
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+function setPosition(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
+}
+
+function drawFood() {
+  const foodElement = createGameElement('div', 'food');
+  setPosition(foodElement, food);
+  board.appendChild(foodElement);
 }
 
 function generateFood() {
-     const x = Math.floor(Math.random()*gridSize) + 1;
-     const y = Math.floor(Math.random() * gridSize) + 1;
-     return { x, y };
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
 }
