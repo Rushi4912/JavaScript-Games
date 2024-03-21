fada6bc16eeb4224cc9dfac26dc602ba59c2d657
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-03-21 21:58:52 +0530

    change

--- snake/script.js
+++ snake/script.js
@@ -1,47 +1,55 @@
 // Define html element
-const board = document.getElementById("game-board");
-let snake = [{ x: 10, y: 10 }];
-let food = generateFood();
-let gridSize = 20;
 
-function draw() {
-  board.innerHTML = "";
-  drawSnake();
-  drawFood();
-}
+document.addEventListener('DOMContentLoaded', () => {
+  draw();
+});
+  const board = document.getElementById("game-board");
+  let snake = [{ x: 10, y: 10 }];
+  let food = generateFood();
+  let gridSize = 20;
+  function draw() {
+    board.innerHTML = "";
+    drawSnake();
+    drawFood();
+  }
+
+  function drawSnake() {
+    snake.forEach((segment) => {
+      const snakeElement = createGameElement("div", "snake");
+      setPosition(snakeElement, segment);
+      board.appendChild(snakeElement);
+    });
+  }
+  function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+  }
+  function setPosition(element, position) {
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+  }
+
+  // Draw food
+  function drawFood() {
+    const foodElement = createGameElement("div", "food");
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+  }
+  function generateFood() {
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return { x, y };
+  }
+
 
 // Draw snake
-function drawSnake() {
-  snake.forEach((segment) => {
-    const snakeElement = createGameElement("div", "snake");
-    setPosition(snakeElement, segment);
-    board.appendChild(snakeElement);
-  });
-}
 
-function createGameElement(tag, className) {
-  const element = document.createElement(tag);
-  element.className = className;
-  return element;
-}
 
-function setPosition(element, position) {
-  element.style.gridColumn = position.x;
-  element.style.gridRow = position.y;
-}
 
-// Draw food
-function drawFood() {
-  const foodElement = createGameElement("div", "food");
-  setPosition(foodElement, food);
-  board.appendChild(foodElement);
-}
 
-function generateFood() {
-  const x = Math.floor(Math.random() * gridSize) + 1;
-  const y = Math.floor(Math.random() * gridSize) + 1;
-  return { x, y };
-}
+
+
 // Call draw function to display the snake and food
  
-draw();
+// draw();
